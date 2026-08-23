b189f233152b0d310af096af61e782ec8a5c8b5a
Ashima <ashima.praj@gmail.com>
2026-08-23 12:29:51 +0530

    Time: 1 ms (99.94%) | Memory: 47.2 MB (13.82%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) 
+    {
+        int i = 0;
+        for(int num : nums)
+        {
+            i ^= num;
+        }
+        return i;
+    }
+}
